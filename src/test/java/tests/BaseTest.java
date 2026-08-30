e945f6ed2a122bad5c7366855521f8c03d8ada66
hk <hassank2413@gmail.com>
2026-08-29 20:23:36 -0500

    Add BaseTest with WebDriver setup and teardown

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
