bb3750165ecfbe446b706cc541b08739ae20ef4e
hk <hassank_23_2@yahoo.com>
2026-09-03 20:26:20 -0500

    Add CartsPage tests for item name, price, and checkout transition

---
+++ src/test/java/tests/CartsTests.java
@@ -0,0 +1,73 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pages.CartsPage;
+import pages.CheckoutPage;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+// Arrange–Act–Assert 
+
+public class CartsTests extends BaseTest {
+
+    LoginPage login;
+    ProductsPage product;
+    CartsPage cart;
+    CheckoutPage checkout;
+
+    private static final String ITEM_TO_TEST = "Sauce Labs Backpack";
+    private static final String CHECKOUT_TITLE = "Checkout: Your Information";
+    private static final String PRICE_TO_TEST = "$29.99";
+
+    @BeforeEach
+    public void setupCartsPage() {
+        login = new LoginPage(driver);
+        product = new ProductsPage(driver);
+        cart = new CartsPage(driver);
+        checkout = new CheckoutPage(driver);
+
+        login.inputUsername("standard_user");
+        login.inputPassword("secret_sauce");
+        login.clickLoginButton();
+
+        product.addItemToCart(ITEM_TO_TEST);
+        product.goToCart();
+    }
+
+    @Test
+    public void testItemNameInCart() {
+
+        // Arrange - done by @beforeeach
+
+        // Act - done by @beforeeach
+
+        // Assert 
+        Assertions.assertEquals(ITEM_TO_TEST, cart.getItemName());
+    }
+
+    @Test
+    public void testItemPriceInCart() {
+
+        // Arrange - done by @beforeeach
+
+        // Act - done by @beforeeach
+        
+        // Assert
+        Assertions.assertEquals(PRICE_TO_TEST, cart.getItemPrice());
+    }
+
+    @Test
+    public void testCheckoutButton() {
+
+        // Arrange - done by @beforeeach
+
+        // Act
+        cart.gotoCheckout();
+        
+        // Assert
+        Assertions.assertEquals(CHECKOUT_TITLE, checkout.getCheckoutPageTitle());
+    }
+}
